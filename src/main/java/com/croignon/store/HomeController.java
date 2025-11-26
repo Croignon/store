ab2de7d2363ea06025977c2ebbaf018bfc924eb1
Croignon <yann.webseg@gmail.com>
2025-11-26 11:21:22 +0100

    added first hello world page

--- src/main/java/com/croignon/store/HomeController.java
+++ src/main/java/com/croignon/store/HomeController.java
@@ -1,5 +1,15 @@
 package com.croignon.store;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
 public class HomeController {
 
+    @RequestMapping("/")
+    public String requestMethodName() {
+        return "index.html";
+    }
+    
 }
